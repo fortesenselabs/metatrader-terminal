a8d73d8e30efedbb5324a9ea504068cd18961be2
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-07-02 19:31:14 +0100

    added system status models

--- metatrader-5/server/application/models/__init__.py
+++ metatrader-5/server/application/models/__init__.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
+from .enum_types import *
 from .mt_models import *
 from .account_info_models import *
 from .exchange_info_models import *
@@ -15,3 +16,23 @@ class ServerTimeResponse(BaseModel):
     timezone: str
     unix_timestamp: int
     server_time: Optional[int] = None
+
+
+class SystemStatus(Enum):
+    NORMAL = 0
+    MAINTENANCE = 1
+
+
+class SystemStatusMessage(Enum):
+    NORMAL = "normal"
+    MAINTENANCE = "System maintenance"
+
+
+class SystemStatusResponse(BaseModel):
+    status: SystemStatus = SystemStatus.NORMAL
+    msg: SystemStatusMessage = SystemStatusMessage.NORMAL
+
+
+#
+# Specify that the time uses standard utc time especially data from metatrader
+#
