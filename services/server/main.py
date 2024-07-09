23b0debb501c9c8bb03d214ebc877eb71eb1c561
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-07-09 22:22:28 +0100

    restructured main

--- services/server/main.py
+++ services/server/main.py
@@ -10,24 +10,29 @@ settings = Settings()
 
 
 async def main():
-    logger.info("MetaTrader 5 API Server is starting up")
     logger.info(f"Settings: {settings.model_dump()}")
 
     server_instance = await SocketIOServerClient.create_server()
-    logger.info("Socket.IO server is running...")
+    logger.info("Created Socket.IO Server Instance...")
+
     dwx_client_params = DWXClientParams(mt_directory_path=settings.METATRADER_FILES_DIR)
 
     try:
+        logger.info("Setting up Event Handlers...")
         await RequestHandler.setup_event_handlers(dwx_client_params, server_instance)
 
+        logger.info("Attaching Application to Server Instance...")
         app = web.Application()
         server_instance.instance.attach(app)
         # socketio_server_instance.instance.start_background_task(background_task)
 
         return app
     except Exception as e:
-        raise ValueError("An error occurred while initializing handlers: ", e) from e
+        message = f"An error occurred while initializing API Server: {e}"
+        logger.exception(message)
+        raise ValueError(message) from e
 
 
 if __name__ == "__main__":
+    logger.info("API Server is starting up")
     web.run_app(main(), port=8000, access_log_format=" :: %r %s %T %t")
