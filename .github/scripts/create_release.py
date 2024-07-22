8d5a6d14d0659d535795fe2572d3589f6addf4c1
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-07-22 21:32:35 +0100

    fix: release creation script

--- .github/scripts/create_release.py
+++ .github/scripts/create_release.py
@@ -2,31 +2,32 @@ from github import Github
 import os
 import re
 
-# Caminho para o diretório atual do script
+# Path to the current script's directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-# Caminho para o setup.py relativo ao diretório do script
+# Path to the setup.py relative to the script's directory
 setup_path = os.path.join(script_dir, "../../clients/metatrader-sockets/setup.py")
 
-# Leitura da versão do setup.py
+# Reading the version from setup.py
 with open(setup_path, "r") as f:
     setup_contents = f.read()
+    print(setup_contents)
 
-match = re.search(r"# Version: ([^\n]+)", setup_contents)
+match = re.search(r"version\s*=\s*['\"]([^'\"]+)['\"]", setup_contents)
 if match:
     setup_version = match.group(1)
 else:
     raise ValueError("Could not find version in setup.py")
 
-# Acessa o repositório atual
+# Access the current repository
 g = Github(os.getenv("GITHUB_TOKEN"))
 repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
 
-# Busca a última release
+# Fetch the latest release
 releases = repo.get_releases()
 latest_release = next((release for release in releases), None)
 
-# Se a versão do setup.py for diferente da última release, cria uma nova release
+# If the setup.py version is different from the latest release, create a new release
 if latest_release is None or latest_release.tag_name != setup_version:
     repo.create_git_ref(ref=f"refs/tags/{setup_version}", sha=os.getenv("GITHUB_SHA"))
     repo.create_git_release(
